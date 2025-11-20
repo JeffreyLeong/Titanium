7a8633f2645d135ea4a6c060e354503790db93fd
dollarortwo <jleong001@gmail.com>
2025-11-19 20:24:28 -0800

    Restructure project for AWS EB

---
+++ Titanium_Site/application.py
@@ -0,0 +1,3 @@
+from titanium import create_app
+
+application = create_app()
--- titanium/__init__.py
+++ titanium/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'enter_compression_formula_2991'
+    app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "dev_key")
 
     from titanium import routes
     app.register_blueprint(routes.bp)
