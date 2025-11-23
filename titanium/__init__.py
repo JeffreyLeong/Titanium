03f8dc3d552c754b8a2f44b05ad8961e0e3ad4f1
dollarortwo <jleong001@gmail.com>
2025-11-22 19:52:34 -0800

    Added car maintenance code

--- titanium/__init__.py
+++ titanium/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_talisman import Talisman
 import os
 
 def create_app():
     app = Flask(__name__)
+    Talisman(app, force_https=True)
     app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "dev_key")
 
     from titanium import routes
