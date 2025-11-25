fdc58f9d158962ff67f229ef6ec2c57e9183a339
dollarortwo <jleong001@gmail.com>
2025-11-24 16:23:53 -0800

    Integrate car_maintenance blueprint

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
 from titanium_site import create_app
 
+# AWS Elastic Beanstalk looks for 'application' variable
 application = create_app()
--- titanium_site/__init__.py
+++ titanium_site/__init__.py
@@ -1,3 +1,14 @@
-from .app import create_app
+from flask import Flask
 
+def create_app():
+    app = Flask(__name__)
 
+    # Register Car Maintenance Blueprint
+    from .car_maintenance.routes import car_bp
+    app.register_blueprint(car_bp, url_prefix="/car")
+
+    # You can register other blueprints, e.g., tip_calculator, core, etc.
+    # from .tip_calculator.routes import tip_bp
+    # app.register_blueprint(tip_bp, url_prefix="/tip")
+
+    return app
